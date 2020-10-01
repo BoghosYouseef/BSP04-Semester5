7b42fa7afb748eeba1ec4c2d31657cc820623aa4
Boghos Youseef <43525406+BoghosYouseef@users.noreply.github.com>
2020-10-01 13:38:48 +0200

    Update scratch.py

--- scratch.py
+++ scratch.py
@@ -2,14 +2,15 @@ from random import randint
 
 
 iteration = 0
-def recursive_like_func(x , term_num):
-    constant = randint(0, 100)
+def recursive_like_func(A, x , term_num):
+    constant = randint(0, 100)   #b
     global iteration
     iteration += 1
-    print(f"Term: {x} + {constant}. Iteration: {iteration}")
-    print(f"x^{iteration} + {constant}")
+    print(f"Term: {x} + {constant}| Iteration: {iteration}")
+    print(f"{A} * x^{iteration} + {constant} = y = {A*x*constant}")
+    print(f"A = {A}, x^{iteration}, b = {constant}, y = {A*x*constant}")
     if iteration == term_num:
         return True
-    return recursive_like_func(((3*x)+constant), term_num)
+    return recursive_like_func(A,((A*x)+constant), term_num)
 
-print(recursive_like_func(1, 100))
+print(recursive_like_func(3,1, 100))
